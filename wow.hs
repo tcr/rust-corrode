abe64a16fa68aced0c8e2c5fe79afaa80d181da0
Tim Ryan <id@timryan.org>
2017-07-19 13:04:05 -0400

    removes wow.hs

--- wow.hs
+++
@@ -1,7 +0,0 @@
-module Test ()
-where
-
-promoteArg :: Result -> Rust.Expr
-promoteArg r = alpha
-    where
-        mkIf c' t' f' = Rust.IfThenElse c' (Rust.Block [] (Just t')) (Rust.Block [] (Just f'))
